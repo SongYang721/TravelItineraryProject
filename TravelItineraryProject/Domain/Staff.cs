345758c8d77d61b94dad3498244b7092522d8bae
RAJARAM NITHESH 2301258F <2301258F@student.tp.edu.sg>
2025-01-29 19:41:45 +0800

    created new role called staff and did the required

--- TravelItineraryProject/Domain/Staff.cs
+++ TravelItineraryProject/Domain/Staff.cs
@@ -8,6 +8,8 @@ namespace TravelItineraryProject.Domain
         public string? Password { get; set; }
         public string? Name { get; set; }
 
+        public string? Email { get; set; }
+
         public List<Itinerary>? Itineraries { get; set; } // = new List<Itinerary>();
         public List<Review>? Reviews { get; set; } // = new List<Review>();
         public List<Booking>? Bookings { get; set; } // = new List<Booking>();
